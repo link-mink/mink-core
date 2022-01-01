bf1c4fd9be6717879eeb598cbef3c1290dc4cfd4
Damir Franusic <damir.franusic@gmail.com>
2022-01-01 17:39:09 +0100

    feat: create single error definition source
    * move all mink error codes to a single header
      file

---
+++ src/include/mink_err_codes.h
@@ -0,0 +1,27 @@
+/*            _       _
+ *  _ __ ___ (_)_ __ | | __
+ * | '_ ` _ \| | '_ \| |/ /
+ * | | | | | | | | | |   <
+ * |_| |_| |_|_|_| |_|_|\_\
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#ifndef MINK_ERROR_CODE_H
+#define MINK_ERROR_CODE_H
+
+namespace mink {
+    namespace error {
+        enum ErrorCode {
+            EC_JSON_MALFORMED       = -1,
+            EC_REQ_TIMEOUT          = -2,
+            EC_GDT_PUSH_FAILED      = -3,
+            EC_AUTH_INVALID_METHOD  = -4,
+            EC_AUTH_FAILED          = -5,
+            EC_UNKNOWN              = -9999
+        };
+    }
+} // namespace mink
+
+#endif /* ifndef MINK_ERROR_CODE_H */
