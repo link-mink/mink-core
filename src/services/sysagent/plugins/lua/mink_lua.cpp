aa0903fa95ed21179bfc8c5a7664bcc535eb4cbe
Damir Franusic <damir.franusic@gmail.com>
2022-04-24 20:11:25 +0200

    feat(ndpi): add plg2plg interface

--- src/services/sysagent/plugins/lua/mink_lua.cpp
+++ src/services/sysagent/plugins/lua/mink_lua.cpp
@@ -9,6 +9,7 @@
  */
 
 #include <iostream>
+#include <gdt.pb.enums_only.h>
 #include <mink_plugin.h>
 #include <json_rpc.h>
 #include <vector>
@@ -21,7 +22,11 @@ using Jrpc = json_rpc::JsonRpc;
 /************/
 /* cmd_call */
 /************/
-extern "C" int mink_lua_cmd_call(void *md, int argc, const char **args) {
+extern "C" int mink_lua_cmd_call(void *md,
+                                 int argc,
+                                 const char **args,
+                                 char ***out,
+                                 int *out_sz) {
     // plugin manager
     mink_utils::PluginManager *pm = static_cast<mink_utils::PluginManager *>(md);
     // argument count check
@@ -29,12 +34,23 @@ extern "C" int mink_lua_cmd_call(void *md, int argc, const char **args) {
     // get command id
     int cmd_id = Jrpc::get_method_id(args[0]);
     // cmd arguments
-    std::vector<std::string> cmd_args;
+    mink_utils::Plugin_args cmd_args;
     for (int i = 1; i < argc; i++) {
         cmd_args.push_back(args[i]);
     }
     // run plugin method
-    return pm->run(cmd_id,
-                   mink_utils::PluginInputData(mink_utils::PLG_DT_SPECIFIC, &cmd_args),
-                   true);
+    int r = pm->run(cmd_id,
+            mink_utils::PluginInputData(mink_utils::PLG_DT_SPECIFIC, &cmd_args),
+            true);
+    // err check, create result
+    if (!r) {
+        *out_sz = cmd_args.size();
+        *out = (char **)malloc(cmd_args.size() * sizeof(char **));
+        for (size_t i = 0; i < cmd_args.size(); i++) {
+            (*out)[i] = (char *)malloc(cmd_args[i].size() + 1);
+            strcpy((*out)[i], cmd_args[i].c_str());
+        }
+    }
+    // return status
+    return r;
 }
